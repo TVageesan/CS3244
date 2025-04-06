448ee82a3cc7c7d26638b386bd74750510e4c144
TVageesan <tvageesan303@gmail.com>
2025-04-06 17:30:38 +0800

    Add geocode support to main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from utils.file_utils import merge_csv
+from utils.file_utils import merge_csv, read_csv
+from utils.calc_distance import add_nearest_mrt
 from preprocessing.clean import clean_data
 from preprocessing.encode import encode_data
 
@@ -12,10 +13,24 @@ def main():
     merged_data.to_csv('output/data.csv', index=False)
     print("Merged data saved to 'data.csv'")
 
-    print("Cleaning data...")
-    
-    cleaned_data = clean_data(merged_data, drop_fields = ['block', 'street_name', 'storey_range', 'lease_commence_date'])
-    
+    print("Geocoding data...")
+    ## Since there's API Limits, try to use the generated geocoded_data set instead of creating yourself
+    geocoded_data = read_csv('output/geocoded_data.csv')
+    enhanced_data = add_nearest_mrt(geocoded_data)
+    cleaned_data = clean_data(enhanced_data)
+
+    # Extract useful fields
+    # Note: Might want to get rid of 'town' field for linear regression,
+    # Or Get rid of 'longitude', 'latitude' for decision tree
+    column_order = [
+        'year', 'month', 
+        'town', 'longitude', 'latitude', 'distance_to_nearest_mrt', 
+        'avg_floor', 'flat_type', 'flat_model', 'floor_area_sqm', 'remaining_lease', 
+        'resale_price'
+    ]
+
+    cleaned_data = cleaned_data[column_order]
+
     cleaned_data.to_csv('output/clean_data.csv', index=False)
     
     print("Cleaned data saved to 'clean_data.csv'")
